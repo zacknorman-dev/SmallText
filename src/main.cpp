f224e18e176c9cd6fece5761c7cd01f5c2f3b0ab
zacknorman-dev <zacknorman-dev@users.noreply.github.com>
2025-12-08 09:40:21 -0800

    v0.21.0 - Crash fix for MQTT-only mode

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "WiFiManager.h"
 #include "OTAUpdater.h"
 
-#define BUILD_NUMBER "v0.20.0"
+#define BUILD_NUMBER "v0.21.0"
 
 // Pin definitions for Heltec Vision Master E290
 #define LORA_CS 8
